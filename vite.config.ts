44ac8afd83cabf2df12a138d8699472d27720072
Alex Kim <alexgkim205@gmail.com>
2023-10-04 02:43:08 -0400

    chore: bundle

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { dependencies } from './package.json';
 function renderChunks(deps: Record<string, string>) {
   const chunks = {};
   Object.keys(deps).forEach((key) => {
-    if (key === "react" || key === "react-dom") return;
+    if (key === "react" || key === "react-dom" || key === '@nextui-org/react') return;
     chunks[key] = [key];
   });
   return chunks;
@@ -20,7 +20,7 @@ export default defineConfig({
     rollupOptions: {
       output: {
         manualChunks: {
-          vendor: ['react', 'react-dom'],
+          vendor: ['react', 'react-dom', '@nextui-org/react'],
           ...renderChunks(dependencies),
         },
       },
