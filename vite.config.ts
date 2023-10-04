64818626a96f26d9ec6228d16f0148367bc1eab6
Alex Kim <alexgkim205@gmail.com>
2023-10-04 03:50:13 -0400

    refactor: srcset images

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,20 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
 import { defineConfig } from 'vite'
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
 import react from '@vitejs/plugin-react-swc'
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
 import { imagetools } from 'vite-imagetools'
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
 import { dependencies } from './package.json';
 
 function renderChunks(deps: Record<string, string>) {
   const chunks = {};
   Object.keys(deps).forEach((key) => {
-    if (key === "react" || key === "react-dom") return;
+    if (key === "react" || key === "react-dom" || key === "@nextui-org/react" || key === "framer-motion") return;
     chunks[key] = [key];
   });
   return chunks;
