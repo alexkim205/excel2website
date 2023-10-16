92d9a96026de376725bf9ab23b7354d4d6e532be
Alex Kim <alexgkim205@gmail.com>
2023-10-16 12:55:30 -0400

    feat: pricing link

--- src/components/Announcement.tsx
+++ src/components/Announcement.tsx
@@ -6,7 +6,7 @@ export function Announcement() {
         <div className="bg-warning font-medium text-lg px-6 w-full py-5 flex justify-center items-center">
             <div className="max-w-[800px] text-center">
                 🔥 Lock in a lifetime deal before pricing changes! $20 for unlimited public dashboards on custom domains for life. <Link
-                href={urls.sign_up()}
+                href={urls.pricing()}
                 underline="always"
                 className="ml-1 text-lg text-primary-600"
                 color="primary"
